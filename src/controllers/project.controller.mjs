1b865b4e7d29a8dcf1e00b8693020e7fa1ff9b5c
soubj <sobujahmed124@gmail.com>
2024-03-10 17:39:19 +0600

    added getProjectById controller

--- src/controllers/project.controller.mjs
+++ src/controllers/project.controller.mjs
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import { Project } from "../models/project.model.mjs";
 import { ApiError } from "../utils/ApiError.mjs";
 import { ApiResponse } from "../utils/ApiResponse.mjs";
@@ -6,6 +7,7 @@ import { asyncHandler } from "../utils/asyncHandler.mjs";
 const getProjectByCategory = asyncHandler(async (req, res) => {
   const { category } = req.params;
   const { page = 1, limit = 8 } = req.query;
+
   if (!category.trim()) {
     throw new ApiError(404, "Category is missing.");
   }
@@ -18,7 +20,7 @@ const getProjectByCategory = asyncHandler(async (req, res) => {
 
   const projectsAggregate = Project.aggregate([{ $match: query }]);
 
-  const options = { page, limit };
+  const options = { page: parseInt(page), limit: parseInt(limit) };
   const projects = await Project.aggregatePaginate(projectsAggregate, options);
   if (!projects) {
     throw new ApiError(500, "No projects found.");
@@ -29,4 +31,20 @@ const getProjectByCategory = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, projects, "Projects fetched successfully."));
 });
 
-export { getProjectByCategory };
+const getProjectById = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+  if (!isValidObjectId(projectId)) {
+    throw new ApiError(400, "Invalid project id");
+  }
+
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "NO project found .");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project fetched successfully."));
+});
+
+export { getProjectByCategory, getProjectById };
--- src/routers/project.routers.mjs
+++ src/routers/project.routers.mjs
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getProjectByCategory } from "../controllers/project.controller.mjs";
+import {
+  getProjectByCategory,
+  getProjectById,
+} from "../controllers/project.controller.mjs";
 
 const router = Router();
 
 router.route("/:category").get(getProjectByCategory);
+router.route("/id/:projectId").get(getProjectById);
 
 export default router;
